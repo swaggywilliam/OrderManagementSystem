5124d9c76692ddf2bc679e800013bd19af315652
swaggywilliam <302913345@qq.com>
2024-10-31 16:10:18 +0800

    1.引入SpringCloud Config 2.引入Bus集成Kafka实现动态刷新配置

--- config-server-9001/src/main/java/com/zhuwl/springcloud/ConfigServerApplication.java
+++ config-server-9001/src/main/java/com/zhuwl/springcloud/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.zhuwl.springcloud;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,6 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class ConfigServerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
